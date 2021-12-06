bba6a8e16b60ade924bd0188669b95b069655b66
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-06 20:43:40 +0600

    Add urls for Snippet views

---
+++ src/textee/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import SnippetDetailView, SnippetCreateView
+
+
+urlpatterns = [
+    path("", SnippetCreateView.as_view(), name="index"),
+    path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
+]
