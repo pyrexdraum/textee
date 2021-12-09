9ec427505ba6dfd14c40ca867b0782d9f296724c
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-09 20:43:42 +0600

    Refactor Forms

--- src/textee/forms.py
+++ src/textee/forms.py
@@ -1,53 +1,55 @@
 from datetime import timedelta
+from typing import Optional
 
 from django import forms
 from django.utils import timezone
-from django.db import models
 
 from .models import Snippet
 
 
 class SnippetForm(forms.ModelForm):
+    NO_TIME = "N"
+    TEN_MINUTES = "10"
+    ONE_HOUR = "1H"
+    ONE_DAY = "1D"
+    ONE_WEEK = "1W"
+    ONE_MONTH = "1M"
+    HALF_YEAR = "6M"
+    ONE_YEAR = "1Y"
+    EXPIRATION_CHOICES = (
+        (NO_TIME, "Не удалять"),
+        (TEN_MINUTES, "10 минут"),
+        (ONE_HOUR, "1 час"),
+        (ONE_DAY, "1 день"),
+        (ONE_WEEK, "1 неделя"),
+        (ONE_MONTH, "1 месяц"),
+        (HALF_YEAR, "Пол года"),
+        (ONE_YEAR, "1 год"),
+    )
+    EXPIRATION_IN_TIMEDELTA = {
+        TEN_MINUTES: timedelta(minutes=10),
+        ONE_HOUR: timedelta(hours=1),
+        ONE_DAY: timedelta(days=1),
+        ONE_WEEK: timedelta(weeks=1),
+        ONE_MONTH: timedelta(days=30),
+        HALF_YEAR: timedelta(days=366 // 2),
+        ONE_YEAR: timedelta(days=366),
+    }
 
-    class Timer(models.TextChoices):
-        FOREVER = ("N", "Не удалять")
-        TEN_MINUTES = ("10", "10 минут")
-        ONE_HOUR = ("1H", "1 час")
-        ONE_DAY = ("1D", "1 день")
-        ONE_WEEK = ("1W", "1 неделя")
-        ONE_MONTH = ("1M", "1 месяц")
-        SIX_MONTHS = ("6M", "6 месяцев")
-        ONE_YEAR = ("1Y", "1 год")
-
-        @classmethod
-        def get_timer(cls, time_code):
-            time_code_in_minutes = {
-                cls.TEN_MINUTES: 10,
-                cls.ONE_HOUR:    60,
-                cls.ONE_DAY:     60 * 24,
-                cls.ONE_WEEK:    60 * 24 * 7,
-                cls.ONE_MONTH:   60 * 24 * 7 * 4,
-                cls.SIX_MONTHS:  60 * 24 * 7 * 4 * 6,
-                cls.ONE_YEAR:    60 * 24 * 7 * 4 * 12,
-            }
-            if time_code == cls.FOREVER:
-                return None
-            elif time_code not in time_code_in_minutes:
-                raise ValueError()
-
-            return timezone.now() + timedelta(minutes=time_code_in_minutes[time_code])
-
-    expiration = forms.TypedChoiceField(
+    expiration = forms.ChoiceField(
         label="Срок действия",
-        choices=Timer.choices,
-        coerce=Timer.get_timer,
-        empty_value=None,
+        choices=EXPIRATION_CHOICES,
         required=False,
     )
 
     class Meta:
         model = Snippet
         fields = ("title", "code", "syntax", "expiration")
-        widgets = {
-            "code": forms.Textarea()
-        }
+        widgets = {"code": forms.Textarea()}
+
+    def clean_expiration(self) -> Optional[timezone.datetime]:
+        time_code = self.cleaned_data["expiration"]
+        if time_code not in self.EXPIRATION_IN_TIMEDELTA:
+            return None
+
+        return timezone.now() + self.EXPIRATION_IN_TIMEDELTA[time_code]
