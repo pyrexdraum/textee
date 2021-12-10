605f7351eabad0763c1fd9414c4cf8f021d37861
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-10 18:57:52 +0600

    Fix tests for SnippetCreateViewTest

--- src/textee/tests/test_views.py
+++ src/textee/tests/test_views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -8,30 +10,37 @@ User = get_user_model()
 
 
 class SnippetCreateViewTest(TestCase):
-
     def test_uses_index_template(self):
         response = self.client.get("/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertTemplateUsed(response, "textee/index.html")
 
     def test_uses_form(self):
         response = self.client.get("/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIsInstance(response.context["form"], SnippetForm)
 
     def test_can_save_post_request(self):
-        self.client.post("/", data={"code": "Just a code.", "syntax": "none"})
+        self.client.post("/", data={"code": "Just a code."})
         self.assertEqual(Snippet.objects.count(), 1)
+
         snippet = Snippet.objects.first()
         self.assertEqual(snippet.code, "Just a code.")
 
-    def test_snippet_owner_is_saved_if_user_is_authenticated(self):
+    def test_snippet_owner_is_set_for_authenticated_user(self):
         user = User.objects.create(username="test")
         self.client.force_login(user)
-        payload = {"code": "123", "syntax": "none"}
-        self.client.post("/", data=payload)
+        self.client.post("/", {"code": "authenticated"})
+
         snippet = Snippet.objects.first()
         self.assertEqual(snippet.owner, user)
 
+    def test_snippet_owner_is_none_if_user_not_authenticated(self):
+        self.client.post("/", {"code": "not authenticated"})
+        snippet = Snippet.objects.first()
+        self.assertIsNone(snippet.owner)
+
     def test_redirects_after_save(self):
-        response = self.client.post("/", data={"code": "1", "syntax": "none"})
-        created_snippet = Snippet.objects.first()
-        self.assertRedirects(response, f"/{created_snippet.url}/")
+        response = self.client.post("/", data={"code": "redirected"})
+        snippet = Snippet.objects.first()
+        self.assertRedirects(response, f"/{snippet.url}/")
