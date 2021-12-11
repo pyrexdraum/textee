cf7071e51af1a68f90c1eb73912b3abf436143a9
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-11 14:55:19 +0600

    Change url path acoounts to auth

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('textee.urls')),
+    path("admin/", admin.site.urls),
+    path("auth/", include("allauth.urls")),
+    path("", include("textee.urls")),
 ]
