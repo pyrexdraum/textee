4f7555b6b1fb7815a9d637806db046fab2e2b5d6
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-15 00:14:04 +0600

    Add an url endpoint for editing snippets

--- src/textee/urls.py
+++ src/textee/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import SnippetCreateView, SnippetDeleteView, SnippetDetailView
+from .views import (
+    SnippetCreateView,
+    SnippetDeleteView,
+    SnippetDetailView,
+    SnippetUpdateView,
+)
 
 urlpatterns = [
     path("", SnippetCreateView.as_view(), name="index"),
     path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
     path("<str:url>/delete/", SnippetDeleteView.as_view(), name="snippet_delete"),
+    path("<str:url>/edit/", SnippetUpdateView.as_view(), name="snippet_edit"),
 ]
