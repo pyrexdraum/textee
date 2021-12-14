5b9e9ea5dac43a0fd1048e25f625eb741369b521
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-14 11:03:12 +0600

    Fix highlighted code display

--- src/textee/service.py
+++ src/textee/service.py
@@ -1,10 +1,10 @@
 from pygments import highlight
-from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
 
 
 def highlight_code(code: str, syntax: str) -> str:
     """Возвращает `подсвеченный` html-тегами code."""
-    lexer = get_lexer_by_name(syntax, encoding='utf-8', stripall=True)
-    formatter = HtmlFormatter(linenos='inline')
+    lexer = get_lexer_by_name(syntax, encoding="utf-8", stripall=True)
+    formatter = HtmlFormatter(linenos="table")
     return highlight(code, lexer, formatter)
