8352034921af60fec9991c7707c7a971ce842c99
Almas Bekbayev <pyrexdraum@gmail.com>
2021-12-14 21:13:18 +0600

    Add url endpoint to delete snippets

--- src/textee/urls.py
+++ src/textee/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import SnippetDetailView, SnippetCreateView
-
+from .views import SnippetCreateView, SnippetDeleteView, SnippetDetailView
 
 urlpatterns = [
     path("", SnippetCreateView.as_view(), name="index"),
     path("<str:url>/", SnippetDetailView.as_view(), name="snippet_detail"),
+    path("<str:url>/delete/", SnippetDeleteView.as_view(), name="snippet_delete"),
 ]
